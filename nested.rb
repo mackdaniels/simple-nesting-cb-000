47c63803bbb318d5304ce607e92dad74f1356c2c
Nicholas Daniel <mackdaniels@protonmail.com>
2018-03-25 15:18:50 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -88,9 +88,14 @@ def adding_matz
           :known_for => "Unix",
           :languages => ["C"]
         }
+
+				:yukihiro_matsumoto => {
+				   :known_for => "Ruby",
+				   :languages => ["LISP", "C"]
+				 }
      }
 
-		programmer_hash[:yukihiro_matsumoto] => "ruby"
+		
 
 end
 
