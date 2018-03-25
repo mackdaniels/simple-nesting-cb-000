5cd42d397ed25ae025a76ace3a1bc83a189f48de
Nicholas Daniel <mackdaniels@protonmail.com>
2018-03-25 15:19:51 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -115,6 +115,8 @@ def changing_alan
           :languages => ["C"]
         }
      }
+
+		 binding.pry
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
 
